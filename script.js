7f5a4db7c318b251a3ed84fda4ba6d6f76fa9037
growbe <growbe@gmail.com>
2021-05-27 23:06:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let card = document.querySelectorAll('say__card');
+
+card[0].style = `
+box-shadow = 15px 25px 60px -10px rgba(0, 0, 0, 0.1);
+`
+card[1].style = `
+box-shadow = 15px 25px 60px -10px rgba(0, 0, 0, 0.1);
+`
+card[2].style = `
+box-shadow = 15px 25px 60px -10px rgba(0, 0, 0, 0.1);
+`
+card[3].style = `
+box-shadow = 15px 25px 60px -10px rgba(0, 0, 0, 0.1);
+`
+for (let i = 0; i < card.length; i++) {
+    
+    
+}
